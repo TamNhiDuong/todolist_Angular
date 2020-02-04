88e835040ab94d7eb8a0c588958628be534563ae
TamNhiDuong <yanxinyee@gmail.com>
2020-02-04 17:29:10 +0200

    handling input, add to array

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -3,13 +3,55 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  todos: object[];
+  todoTitle: string;
+  todoId: number;
+  id: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.todoId = 4;
+    this.todoTitle = '';
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'Learn Angular',
+        'completed': false,
+        'editing': false
+      },
+      {
+        'id': 2,
+        'title': 'Land first job',
+        'completed': false,
+        'editing': false
+      },
+      {
+        'id': 3,
+        'title': 'Calm soul',
+        'completed': false,
+        'editing': false
+      }
+    ]
   }
+addTodo(): void {
+  if(this.todoTitle.trim().length === 0) {
+    return;
+  }
+  this.todos.push({
+    id: this.todoId,
+    title: this.todoTitle,
+    completed: false,
+    editing: false
+  })
+  this.todoId++;
+  this.todoTitle= '';
+}
 
+deleteTodo(id: number): void {
+  this.todos = this.todos.filter(todo => todo.id !== id)
+}
 }
