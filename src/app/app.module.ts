491e43c50ccd1908fc29a1890e17c2d259657202
TamNhiDuong <yanxinyee@gmail.com>
2020-02-05 13:43:58 +0200

    Split components, passing state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormModule, FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AutofocusModule } from 'angular-autofocus-fix';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
