0af9252f8c036c4c12d79d2597638ff8d8afb493
TamNhiDuong <yanxinyee@gmail.com>
2020-02-05 11:46:25 +0200

    adding animation

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../interfaces/Todo';
 
+import {trigger, transition, style, animate} from '@angular/animations';
+
 @Component({
   selector: 'todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
+  animations: [
+    trigger('fade', [
+
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(30px)'}),
+        animate(200, style({ opacity: 1, transform: 'translateY(0px)'}))
+      ]),
+
+      transition(':leave', [
+        animate(200, style({ opacity: 0, transform: 'translateX(30px)'}))
+      ]),
+
+    ])
+  ]
 })
 export class TodoListComponent implements OnInit {
   todos: Todo[];
