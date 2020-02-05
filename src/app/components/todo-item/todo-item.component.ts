f10bc22e3c09e2283133e2f24ea4ed377ec1bfc9
TamNhiDuong <yanxinyee@gmail.com>
2020-02-05 14:51:20 +0200

    event emitter

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Todo } from 'src/app/interfaces/Todo';
 
 @Component({
@@ -8,9 +8,31 @@ import { Todo } from 'src/app/interfaces/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() checkedItem= new EventEmitter;
+  @Output() editingItem = new EventEmitter;
+  @Output() cancelEditItem = new EventEmitter;
+  @Output() deletedItem = new EventEmitter;
   constructor() { }
 
   ngOnInit() {
   }
+
+  editingTodo(todo: Todo): void {
+    this.editingItem.emit(todo)
+  }
+
+  doneEdit(todo: Todo): void {
+    this.checkedItem.emit(todo);
+  }
+
+  cancelEdit(todo: Todo): void {
+    this.cancelEditItem.emit(todo);
+  }
+
+  deleteTodo(id: number): void {
+    this.deletedItem.emit(id);
+  }
+
+
 
 }
